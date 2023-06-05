30beef3c86db64fc131e80769d656b5943309411
kimyezzang97 <kimyezzang97@gmail.com>
2023-06-06 08:48:42 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+// src/App.vue
+<script setup>
+import TheHeader from '@/layouts/TheHeader.vue';
+import TheView from '@/layouts/TheView.vue';
+</script>
+
+<template>
+	<TheHeader></TheHeader>
+
+	<TheView></TheView>
+</template>
---
+++ src/layouts/TheVIew.vue
@@ -0,0 +1,12 @@
+// src/components/TheView.vue
+<template>
+	<main>
+		<div class="container py-4">
+			<h2>Hello Vue3</h2>
+		</div>
+	</main>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
